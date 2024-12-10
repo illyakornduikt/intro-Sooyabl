46071a944c2b5602437912edabfa7b0da9068b79
Sooyabl <dashamyza2006@gmail.com>
2024-12-10 23:18:52 +0200

    edits bug

--- javaSwing/src/tile/TileManager.java
+++ javaSwing/src/tile/TileManager.java
@@ -16,7 +16,7 @@ public class TileManager {
         tile = new Tile[6];
         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
         getTileImage();
-        loadMap("javaSwingProject/res/maps/world01.txt");
+        loadMap("javaSwing/res/maps/world01.txt");
     }
     public void getTileImage(){
         try {
